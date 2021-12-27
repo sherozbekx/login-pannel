3c9fa5e94e9f019834bfc4b42f296352f2c161e3
KhSherozbek1 <89767832+KhSherozbek1@users.noreply.github.com>
2021-12-27 14:33:02 +0500

    Update user.php

--- user.php
+++ user.php
@@ -10,7 +10,7 @@ session_start();
     <meta charset="UTF-8">
     <title><?php echo $_SESSION['username'] ?> | Home</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/user.css">
+    <link rel="stylesheet" href="user.css">
 </head>
 <body>
     <nav class="navbar">
